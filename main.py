5dc1208d5f9511708c3c56e79beea411ea9f9e0d
Vishnu-sotaog <vvenkannagari@sotaog.com>
2025-02-13 14:08:36 -0600

    case studies added

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ def read_root():
 @app.get("/test")
 def test():
     return {"message": "Hello from EC2!"}
+
+
+@app.post("/strategic-model")
+def strategicModel():
+    return {"message": "Success"}
+
+
+@app.post("/operational-model")
+def operationalModel():
+    return {"message": "Success"}
